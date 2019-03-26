4b6f38fb04d709736e68edec76cbe5d96d51e920
SSSO <thdud00627@gmail.com>
2019-03-26 19:47:20 +0900

    add Echo project

--- 20190326_Echo/src/Echo.java
+++ 20190326_Echo/src/Echo.java
@@ -1,9 +1,16 @@
+import java.util.*;
 
 public class Echo {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("소리를 질러요~ (종료 : exit) : ");
+			String echo = sc.nextLine();
+			if(echo.equals("exit")) {
+				System.out.println("종료합니다^^");
+				System.exit(0);
+			}
+			System.out.println(echo + "~~");
+		}
 	}
-
 }
