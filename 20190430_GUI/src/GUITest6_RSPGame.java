44755bb3fbb0788aff3ee2b08e88584855f52983
SSSO <thdud00627@gmail.com>
2019-05-23 18:53:31 +0900

    가위바위보 그림 추가

--- 20190430_GUI/src/GUITest6_RSPGame.java
+++ 20190430_GUI/src/GUITest6_RSPGame.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import javax.swing.*;
 
 public class GUITest6_RSPGame {
+	static String[] fileName = {"src/images/scissors.png", "src/images/rock.png", "src/images/paper.png"};
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
@@ -9,15 +10,15 @@ public class GUITest6_RSPGame {
 		frame.setPreferredSize(new Dimension(400, 600));
 		
 		JLabel result = new JLabel("Win");
-		ImageIcon computerImage = new ImageIcon("");
+		ImageIcon computerImage = new ImageIcon(fileName[1]);
 		JLabel computerLabel = new JLabel(computerImage);
 		
-		ImageIcon scissorsImage = new ImageIcon("");
+		ImageIcon scissorsImage = new ImageIcon(fileName[0]);
 		JButton scissorsButton = new JButton(scissorsImage);
-		ImageIcon rockImage = new ImageIcon("");
-		JButton rockButton = new JButton(scissorsImage);
-		ImageIcon paperImage = new ImageIcon("");
-		JButton paperButton = new JButton(scissorsImage);
+		ImageIcon rockImage = new ImageIcon(fileName[1]);
+		JButton rockButton = new JButton(rockImage);
+		ImageIcon paperImage = new ImageIcon(fileName[2]);
+		JButton paperButton = new JButton(paperImage);
 		
 		panel.add(result);
 		panel.add(computerLabel);
