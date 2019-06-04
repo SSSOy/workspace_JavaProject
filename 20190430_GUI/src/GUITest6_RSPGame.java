c20c0dbca1be764f4bd731b783b26b8289d07e60
SSSO <thdud00627@gmail.com>
2019-06-04 19:16:03 +0900

    GUI 가위바위보

--- 20190430_GUI/src/GUITest6_RSPGame.java
+++ 20190430_GUI/src/GUITest6_RSPGame.java
@@ -1,8 +1,16 @@
+import java.util.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class GUITest6_RSPGame {
 	static String[] fileName = {"src/images/scissors.png", "src/images/rock.png", "src/images/paper.png"};
+	static String[] answerString = {"사용자 짐", "비김", "사용자가 이김"};
+	protected static int computer;
+	protected static int player;
+	protected static int answer;
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
@@ -20,6 +28,28 @@ public class GUITest6_RSPGame {
 		ImageIcon paperImage = new ImageIcon(fileName[2]);
 		JButton paperButton = new JButton(paperImage);
 		
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Object obj = e.getSource();
+				if(obj == scissorsButton) 
+					player = 1;
+				else if(obj == rockButton) 
+					player = 2;
+				else
+					player = 3;
+				
+				computer = makeComputer();
+				ImageIcon computerImage = new ImageIcon(fileName[computer - 1]);
+				computerLabel.setIcon(computerImage);
+				answer = compare(player, computer);
+				result.setText(answerString[answer]);
+			}
+		};
+		
+		scissorsButton.addActionListener(al);
+		rockButton.addActionListener(al);
+		paperButton.addActionListener(al);
+		
 		panel.add(result);
 		panel.add(computerLabel);
 		panel.add(scissorsButton);
@@ -30,5 +60,20 @@ public class GUITest6_RSPGame {
 		frame.pack();
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private static int compare(int p, int c) {
+		if ((p + 1) % 3 == c % 3) 
+			return 0;
+		else if (p == c) 
+			return 1;
+		else 
+			return 2;
+	}
+
+	private static int makeComputer() {
+		Random random = new Random();
+		int c = random.nextInt(3 - 1 + 1) + 1; // 1~3 임의의 수
+		return c;
 	}
 }
